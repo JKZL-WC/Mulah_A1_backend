b03facf8fc9e7131d8abd70dd45e9fe6ddd4e934
JKZL-WC <leejj-pm20@student.tarc.edu.my>
2025-01-08 12:43:56 +0800

    Initial commit for assessment

--- scraper.py
+++ scraper.py
@@ -10,9 +10,10 @@ import time
 def fetch_articles(url, date_limit):
     # setup Selenium WebDriver
     options = Options()
-    options.add_argument("--headless") 
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--headless")  
+    options.add_argument("--disable-gpu")  
+    options.add_argument("--no-sandbox")  
+    options.add_argument("--disable-dev-shm-usage")  
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver.get(url)
